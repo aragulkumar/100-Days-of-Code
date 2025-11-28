7b0cd5ae83a0a9bb852e527d8d21157a0ec031ca
aragulkumar <aragulkumar11@gmail.com>
2025-11-28 23:44:59 +0530

    34th day completed

--- Day-34/quizzler-app-start/ui.py
+++ Day-34/quizzler-app-start/ui.py
@@ -38,15 +38,39 @@ class QuizInterface:
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
-        self.false_button = Button(image=self.false_image, highlightthickness=0, command=Fl)
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.score_label.config(text=f"score: {self.quiz.score} ")
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text = "You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
+         
